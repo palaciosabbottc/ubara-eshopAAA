6be0eaa86d0213f00e08d4a3303ce7b26c66a16a
Cristobal Palacios <toto.palacios.a@gmail.com>
2025-05-16 11:04:00 -0400

    feat: add @supabase/ssr dependency and update layout metadata fetching logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,30 +3,32 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { LayoutClient } from "./layout-client"
-import { headers } from "next/headers"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
+import { createClient } from "@/utils/supabase/server"
 
+// Define the Inter font with Latin subset
 const inter = Inter({ subsets: ["latin"] })
 
+// Function to fetch metadata from the database
 async function getMetadata() {
-  const supabase = createServerComponentClient({ cookies })
+  const supabase = await createClient()
   const { data } = await supabase.from('site_config').select('store_name, store_description').single()
-  return data || { store_name: 'Ubara', store_description: 'Cerámica Artesanal' }
+  return data || { store_name: 'Ubara', store_description: 'Cuadros Texturizados' }
 }
 
+// Function to generate metadata for the page
 export async function generateMetadata(): Promise<Metadata> {
   const { store_name, store_description } = await getMetadata()
   
   return {
     title: store_name,
     description: store_description,
-    icons: {
-      icon: "/favicon.ico",
-    },
+    //icons: {
+    //  icon: "/favicon.ico",
+    //},
   }
 }
 
+// Root layout component that wraps the entire application
 export default function RootLayout({
   children,
 }: {
